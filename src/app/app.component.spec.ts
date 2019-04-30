c7297152ca1f92c4ad96c1b8f9bd5c72b2abd8e1
Pradhan Rishi Sharma <pradhanrishi10@gmail.com>
2019-04-30 18:01:25 +0530

    fix: delete useless test cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mat-nav-bar'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('mat-nav-bar');
-  });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to mat-nav-bar!');
-  });
 });
